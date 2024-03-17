0ec9b4a982962c73155c11b6959c2a249701e3fb
Kamil Lenczewski <142166119+kamillenczewski@users.noreply.github.com>
2024-03-17 23:14:47 +0100

    Update files_generator.py

--- files_generator.py
+++ files_generator.py
@@ -1,6 +1,15 @@
 from random import randint, choices
-from os import remove as remove_file
+from os import remove as os_remove_file
 from os import system
+from shutil import copyfile
+
+STORAGE_FILE = "STORAGE_FILE.txt"
+
+def remove_file(file_name):
+    try:
+        os_remove_file(file_name)
+    except FileNotFoundError:
+        print(f"There is no file with name: '{file_name}'.")
 
 def get_possible_chars():
     possible_small_letters = [chr(i) for i in range(ord("a"), ord("z") + 1)]
@@ -13,13 +22,18 @@ def get_possible_chars():
 def chars_to_str(chars):
     return "".join(chars)
 
-def generate_file_name(possible_chars):
-    random_length = randint(0, 100)
+def generate_random_string(possible_chars):
+    random_length = randint(5, 15)
     random_chars = choices(population=possible_chars, k=random_length)
-    text = chars_to_str(random_chars)
-    file_name = text + ".txt"
+    string = chars_to_str(random_chars)
 
-    return file_name
+    return string
+
+def generate_random_strings(possible_chars, amount):
+    return [generate_random_string(possible_chars) for _ in range(amount)]
+
+def generate_file_name(possible_chars):
+    return generate_random_string(possible_chars) + ".txt"
 
 def generate_file_names(possible_chars, amount):
     return [generate_file_name(possible_chars) for _ in range(amount)]
@@ -35,8 +49,8 @@ def write_files(file_names):
 def add_new_line_char_to_strings(strings):
     return [string + "\n" for string in strings]
 
-def write_files_data(file_names):
-    with open("data_file.txt", "w") as file:
+def write_storage_file(file_names):
+    with open(STORAGE_FILE, "w") as file:
         for file_name in file_names:
             file.write(file_name + "\n")
 
@@ -75,8 +89,8 @@ def get_action_index():
     
     return -1
 
-def read_files_data():
-    with open("data_file.txt", "r") as file:
+def read_storage_file():
+    with open(STORAGE_FILE, "r") as file:
         lines = file.readlines()
 
     return [line.replace("\n", "") for line in lines]
@@ -85,14 +99,18 @@ def execute_action_1():
     amount = get_files_amount()
     possible_chars = get_possible_chars()
     file_names = generate_file_names(possible_chars, amount)
-    write_files_data(file_names)
+    write_storage_file(file_names)
     write_files(file_names)
 
 def execute_action_2():
-    file_names = read_files_data()
-    remove_files(file_names + ["data_file.txt"])
+    file_names = read_storage_file()
+    remove_files(file_names + [STORAGE_FILE])
+
+def clone_file(new_path, source_file_path=__file__):
+    copyfile(source_file_path, new_path)
 
 if __name__ == "__main__":
     match(get_action_index()):
         case 1: execute_action_1()
         case 2: execute_action_2()
+    clone_file("XDDDD.py")
